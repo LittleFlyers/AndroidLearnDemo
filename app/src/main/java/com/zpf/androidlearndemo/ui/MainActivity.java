1d2185eed2abcd7de2f80cd1996fc478dbd42b1c
LittleFlyers <2538285284@qq.com>
2020-04-15 23:20:06 +0800

    Build the basic homepage listview

--- app/src/main/java/com/zpf/androidlearndemo/ui/MainActivity.java
+++ app/src/main/java/com/zpf/androidlearndemo/ui/MainActivity.java
@@ -3,8 +3,13 @@ package com.zpf.androidlearndemo.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.zpf.androidlearndemo.R;
+import com.zpf.androidlearndemo.ui.adapter.MainListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        ListView listView = findViewById(R.id.listview_main);
+        MainListAdapter.MainItem item = new MainListAdapter.MainItem(R.drawable.img_viewpaper, "ViewPaper 示例");
+        List<MainListAdapter.MainItem> list = new ArrayList<>();
+        list.add(item);
+        MainListAdapter adapter = new MainListAdapter(this, R.layout.listview_main, list);
+        listView.setAdapter(adapter);
     }
 }
--- app/src/main/java/com/zpf/androidlearndemo/ui/adapter/MainListAdapter.java
+++ app/src/main/java/com/zpf/androidlearndemo/ui/adapter/MainListAdapter.java
@@ -1,18 +1,40 @@
 package com.zpf.androidlearndemo.ui.adapter;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class MainListAdapter extends BaseAdapter {
-    @Override
-    public int getCount() {
-        return 0;
+import androidx.annotation.NonNull;
+
+import com.zpf.androidlearndemo.R;
+
+import java.util.List;
+
+public class MainListAdapter extends ArrayAdapter<MainListAdapter.MainItem> {
+
+    private int mLayoutId;
+
+    private List<MainItem> mItems;
+
+    public MainListAdapter(Context context, int layoutId, List<MainItem> items) {
+        super(context, layoutId, items);
+        mLayoutId = layoutId;
+        mItems = items;
     }
 
     @Override
-    public Object getItem(int position) {
-        return position;
+    public int getCount() {
+        return mItems.size();
+    }
+
+    @Override
+    public MainItem getItem(int position) {
+        return mItems.get(position);
     }
 
     @Override
@@ -20,8 +42,34 @@ public class MainListAdapter extends BaseAdapter {
         return position;
     }
 
+
+    @NonNull
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        MainItem item = mItems.get(position);
+        @SuppressLint("ViewHolder") View view = LayoutInflater.from(getContext()).inflate(mLayoutId, parent, false);
+        ImageView itemHead = view.findViewById(R.id.list_main_item_head_img);
+        TextView itemText = view.findViewById(R.id.list_main_item_text);
+        itemHead.setImageResource(item.getItemHead());
+        itemText.setText(item.getItemText());
+        return view;
+    }
+
+    public static class MainItem {
+        int itemHead;
+        String itemText;
+
+        public MainItem(int itemHead, String itemText) {
+            this.itemHead = itemHead;
+            this.itemText = itemText;
+        }
+
+        public int getItemHead() {
+            return itemHead;
+        }
+
+        public String getItemText() {
+            return itemText;
+        }
     }
 }
